0b33dbdb37b7e903b320b20ba4179e65870842d5
Rafał Wojdyła <omeg@invisiblethingslab.com>
2015-06-19 01:57:35 +0200

    more flexible wix configuration

--- scripts/wix.bat
+++ scripts/wix.bat
@@ -7,7 +7,13 @@ SET MSIARCH=%DDK_ARCH%
 IF "%WIN_BUILD_TYPE%"=="chk" (SET MSIBUILD=_debug) ELSE (SET MSIBUILD=)
 SET MSIOS=%DDK_DIST%
 
-SET MSINAME=%COMPONENT%-%MSIOS%%MSIARCH%%MSIBUILD%.msm
+:: msm or msi extension is the script's parameter
+if "%1"=="msm" (
+    SET MSINAME=%COMPONENT%-%MSIOS%%MSIARCH%%MSIBUILD%.msm
+) ELSE (
+    SET MSINAME=%COMPONENT%-%MSIOS%%MSIARCH%-%VERSION%%MSIBUILD%.msi
+)
 
 "%WIX%\candle" installer.wxs -arch %MSIARCH% -ext "%WIX%\WixUIExtension.dll" -ext "%WIX%\WixDifxAppExtension.dll"
 "%WIX%\light.exe" -o %MSINAME% installer.wixobj %DIFXLIB% -ext "%WIX%\WixUIExtension.dll" -ext "%WIX%\WixDifxAppExtension.dll"
+exit /b 0
