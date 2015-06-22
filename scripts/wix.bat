0df90e00535e9190e86bddd4aa63109398fb8205
Rafał Wojdyła <omeg@invisiblethingslab.com>
2015-06-23 01:34:42 +0200

    support building multiple installers

--- scripts/wix.bat
+++ scripts/wix.bat
@@ -9,11 +9,33 @@ SET MSIOS=%DDK_DIST%
 
 :: msm or msi extension is the script's parameter
 if "%1"=="msm" (
-    SET MSINAME=%COMPONENT%-%MSIOS%%MSIARCH%%MSIBUILD%.msm
+    SET MSISUFFIX=-%MSIOS%%MSIARCH%%MSIBUILD%.msm
 ) ELSE (
-    SET MSINAME=%COMPONENT%-%MSIOS%%MSIARCH%-%VERSION%%MSIBUILD%.msi
+    SET MSISUFFIX=-%MSIOS%%MSIARCH%-%VERSION%%MSIBUILD%.msi
 )
 
-"%WIX%\candle" installer.wxs -arch %MSIARCH% -ext "%WIX%\WixUIExtension.dll" -ext "%WIX%\WixDifxAppExtension.dll"
-"%WIX%\light.exe" -o %MSINAME% installer.wixobj %DIFXLIB% -ext "%WIX%\WixUIExtension.dll" -ext "%WIX%\WixDifxAppExtension.dll"
+:: Iterate over all installer source files.
+for /R %%f in (*.wxs) do call :build %%~nf
+
+:: Return 0 without checking errorlevel because wix warnings can cause it to return nonzero values.
+:: If there's an error we catch it below.
 exit /b 0
+
+:build
+set FILENAME=%1
+
+set MSINAME=%FILENAME%%MSISUFFIX%
+
+"%WIX%\candle" %FILENAME%.wxs -arch %MSIARCH% -ext "%WIX%\WixUIExtension.dll" -ext "%WIX%\WixDifxAppExtension.dll"
+"%WIX%\light.exe" -o %MSINAME% %FILENAME%.wixobj %DIFXLIB% -ext "%WIX%\WixUIExtension.dll" -ext "%WIX%\WixDifxAppExtension.dll"
+
+:: FIXME: This is not an ideal way to check for errors because the output file may be created
+:: even if wix fails to merge something in. We can't rely on wix warnings (errorlevel) because
+:: some of them are unavoidable (eg. when merging MSVCRT redistributables).
+if not exist %MSINAME% goto :error
+:: return
+goto :eof
+
+:error
+echo [!] Building %MSINAME% FAILED
+exit 1
