798eb561681d6eef85657ac2112b142d754064e0
Rafał Wojdyła <omeg@invisiblethingslab.com>
2015-07-09 16:26:22 +0200

    sign drivers and driver catalogs

--- scripts/sign.bat
+++ scripts/sign.bat
@@ -4,6 +4,6 @@
 
 IF NOT EXIST SIGN_CONFIG.BAT GOTO DONT_SIGN
 
-for /R %%f in (*.dll;*.exe;*.msi) do %SIGNTOOL% sign /v %CERT_CROSS_CERT_FLAG% /f "%CERT_FILENAME%" %CERT_PASSWORD_FLAG% /t http://timestamp.verisign.com/scripts/timestamp.dll "%%f"
+for /R %%f in (*.dll;*.exe;*.msi;*.sys;*.cat) do %SIGNTOOL% sign /v %CERT_CROSS_CERT_FLAG% /f "%CERT_FILENAME%" %CERT_PASSWORD_FLAG% /t http://timestamp.verisign.com/scripts/timestamp.dll "%%f"
 
 :DONT_SIGN
