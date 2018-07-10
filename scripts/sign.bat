b17d4f148cf8665494b92bc8eb2b35fb92ac7fb7
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2018-07-10 13:24:13 +0200

    Use sha256 for certificate, don't sign already signed files
    Now we include some of drivers signed by The Linux Foundation, there is
    no reason for adding local signatures for them, or even worse -
    replacing it.

--- scripts/sign.bat
+++ scripts/sign.bat
@@ -2,8 +2,20 @@
 :: called from Makefile.Windows at the end of dist-package
 @ECHO OFF
 
-IF NOT EXIST SIGN_CONFIG.BAT GOTO DONT_SIGN
+IF NOT EXIST SIGN_CONFIG.BAT GOTO END
 
-for /R %%f in (*.dll;*.exe;*.msi;*.sys;*.cat) do %SIGNTOOL% sign /v %CERT_CROSS_CERT_FLAG% /f "%CERT_FILENAME%" %CERT_PASSWORD_FLAG% /t http://timestamp.verisign.com/scripts/timestamp.dll "%%f"
+for /R %%f in (*.dll;*.exe;*.msi;*.sys;*.cat) do call :sign "%%f"
 
-:DONT_SIGN
+goto :END
+
+:sign
+
+:: don't sign again if already properly signed
+%SIGNTOOL% verify /pa "%1"
+if not errorlevel 1 goto :eof
+
+%SIGNTOOL% sign /v %CERT_CROSS_CERT_FLAG% /f "%CERT_FILENAME%" %CERT_PASSWORD_FLAG% /tr http://timestamp.digicert.com /td sha256 /fd sha256 "%1"
+:: return
+goto :eof
+
+:END
